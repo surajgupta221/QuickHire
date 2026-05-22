e80e29c09dece612fa89189e21a84093095de68f
suraj gupta <suraj.gupta221@gmail.com>
2026-05-22 16:27:19 +0530

    Add Vercel URL to CORS

--- QuickHire/main.py
+++ QuickHire/main.py
@@ -23,7 +23,11 @@ app = FastAPI(
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://quickhire.vercel.app",
+        "*"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
